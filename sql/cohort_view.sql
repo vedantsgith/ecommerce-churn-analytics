3a4766ca5433f02a6421e13aa3bac8b90c1bf19c
Vedant Parasrampuria <vedant7044@gmail.com>
2026-08-25 20:06:11 +0530

    Fix cohort view to use customer_unique_id; reveals severe month-1 retention drop-off

--- sql/cohort_view.sql
+++ sql/cohort_view.sql
@@ -1,26 +1,28 @@
 CREATE VIEW customer_cohorts AS
 WITH first_purchase AS (
     SELECT
-        customer_id,
-        DATE_FORMAT(MIN(order_purchase_timestamp), '%Y-%m-01') AS cohort_month
-    FROM orders
-    WHERE order_status = 'delivered'
-    GROUP BY customer_id
+        c.customer_unique_id,
+        DATE_FORMAT(MIN(o.order_purchase_timestamp), '%Y-%m-01') AS cohort_month
+    FROM customers c
+    JOIN orders o ON c.customer_id = o.customer_id
+    WHERE o.order_status = 'delivered'
+    GROUP BY c.customer_unique_id
 ),
 activity AS (
     SELECT
-        o.customer_id,
+        c.customer_unique_id,
         fp.cohort_month,
         DATE_FORMAT(o.order_purchase_timestamp, '%Y-%m-01') AS order_month
     FROM orders o
-    JOIN first_purchase fp ON o.customer_id = fp.customer_id
+    JOIN customers c ON o.customer_id = c.customer_id
+    JOIN first_purchase fp ON c.customer_unique_id = fp.customer_unique_id
     WHERE o.order_status = 'delivered'
 )
 SELECT
     cohort_month,
     order_month,
     PERIOD_DIFF(DATE_FORMAT(order_month, '%Y%m'), DATE_FORMAT(cohort_month, '%Y%m')) AS month_number,
-    COUNT(DISTINCT customer_id) AS active_customers
+    COUNT(DISTINCT customer_unique_id) AS active_customers
 FROM activity
 GROUP BY cohort_month, order_month
 ORDER BY cohort_month, order_month;
